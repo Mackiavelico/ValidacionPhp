91364e6db91757ad599b6642c02f5ec6ba8d6496
Mackiavelico <jose.m.montecinos.gonzalez@gmail.com>
2023-06-13 14:09:14 -0400

    5
    mas

--- HomeIntranet.php
+++ HomeIntranet.php
@@ -4,17 +4,23 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
 </head>
+
 <body>
     <header>
-        <h4>INTRANET JUEGOS</h4>
+        <h4>INTRANET JUEGOS**</h4>
     </header>
     <hr>
-    
+    <div>
+        <a href="01-loginPhp/index.php">Volver al Inicio</a>
+        <a href="#">logo</a>
+    </div>
 </body>
+
 </html>
